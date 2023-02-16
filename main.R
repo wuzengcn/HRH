54234474e9022f93f7b4e2686134c9663b55f626
wuzengcn <58857805+wuzengcn@users.noreply.github.com>
2023-02-16 17:50:05 -0500

    Update main.R

--- main.R
+++ main.R
@@ -12,17 +12,12 @@ source("sim-model.R")
 
 res <- simu_model(10000)
 
-sapply(res[[1]], mean)
-quantile(res[[1]]$V16, c(0.025, 0.5, 0.975))
+quantile(res[[1]]$V16, c(0.025, 0.975))
 
-sapply(res[[2]], mean)
-quantile(res[[2]]$V16, c(0.025, 0.5, 0.975))
+quantile(res[[2]]$V16, c(0.025, 0.975))
 
-sapply(res[[3]], mean)
-quantile(res[[3]]$V16, c(0.025, 0.5, 0.975))
+quantile(res[[3]]$V16, c(0.025, 0.975))
 
-sapply(res[[4]], mean)
-quantile(res[[4]]$V16, c(0.025, 0.5, 0.975))
+quantile(res[[4]]$V16, c(0.025, 0.975))
 
-sapply(res[[5]], mean)
-quantile(res[[5]]$V16, c(0.025, 0.5, 0.975))
+quantile(res[[5]]$V16, c(0.025, 0.975))
